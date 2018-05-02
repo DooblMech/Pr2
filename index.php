d43ecfd66af7d3c101e67bc10fb5bb68b4d7e098
Anton <wrugleys@yandex.ru>
2018-05-02 11:45:41 +0300

    2

--- index.php
+++ index.php
@@ -5,9 +5,16 @@ spl_autoload_register(['Autoload', 'loader']);
 
 $item = require_once(__DIR__ . '/data.php');
 foreach ($item as $key) {
+    $da1 = (new \app\CV)->buildHead($key);
+    echo($da1. '<br>');
+    foreach ($key as $key1) {
 
-    $da = (new \app\CV($key))->buildContent();
 
+            $da = (new \app\CV)->buildContent($key1);
+
+       // echo($da);
+
+    }
 }
 
-//var_dump($item);
+
